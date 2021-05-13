632fd3ab049e82258fde73d918300d51b080d411
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-05-13 14:09:47 +0000

    Backport 1f47c18ebfbf6c50eb0de87539fde8b79f50aea7
     more data in events

--- runtime/src/tracing.rs
+++ runtime/src/tracing.rs
@@ -3,9 +3,8 @@
 use crate::{Context, Opcode, Stack, Memory, Capture, ExitReason, Trap};
 use crate::{H160, U256};
 
-
 #[cfg(feature = "tracing")]
-environmental::environmental!(hook: dyn EventListener + 'static);
+environmental::environmental!(listener: dyn EventListener + 'static);
 
 #[cfg(feature = "tracing")]
 pub trait EventListener {
@@ -20,10 +19,14 @@ pub enum Event<'a> {
     Step {
         context: &'a Context,
         opcode: Opcode,
+        position: &'a Result<usize, ExitReason>,
         stack: &'a Stack,
         memory: &'a Memory
     },
-    StepResult (&'a Result<(), Capture<ExitReason, Trap>>),
+    StepResult {
+        result: &'a Result<(), Capture<ExitReason, Trap>>,
+        return_value: &'a [u8],
+    },
     SLoad {
         address: H160,
         index: U256,
@@ -39,7 +42,7 @@ pub enum Event<'a> {
 impl<'a> Event<'a> {
     #[cfg(feature = "tracing")]
     pub(crate) fn emit(self) {
-        hook::with(|hook| hook.event(self));
+        listener::with(|listener| listener.event(self));
     }
 
     #[cfg(not(feature = "tracing"))]
@@ -51,8 +54,8 @@ impl<'a> Event<'a> {
 /// Run closure with provided listener.
 #[cfg(feature = "tracing")]
 pub fn using<R, F: FnOnce() -> R>(
-    listener: &mut (dyn EventListener + 'static),
+    new: &mut (dyn EventListener + 'static),
     f: F
 ) -> R {
-    hook::using(listener, f)
+    listener::using(new, f)
 }
